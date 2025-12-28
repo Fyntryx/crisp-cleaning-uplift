fdb3267ce9d9389c2f82657bdbcb15af6bc745a3
Sohaib <180877552+Sohaib-Kamal04@users.noreply.github.com>
2025-12-28 17:59:34 +0500

    Update index.ts

--- src/sanity/schemaTypes/index.ts
+++ src/sanity/schemaTypes/index.ts
@@ -1,7 +1,8 @@
 import { type SchemaTypeDefinition } from "sanity";
 import faq from "./faq";
 import review from "./review";
+import imageeffect from "./imageeffect";
 
 export const schema: { types: SchemaTypeDefinition[] } = {
-  types: [faq, review],
+  types: [faq, review, imageeffect],
 };
