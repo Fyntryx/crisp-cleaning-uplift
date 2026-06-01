3fc2366589b8a29a99a95c9b8b695425ec16ba2b
Fyntryx <adnan@fyntryx.org>
2026-06-01 16:50:11 +0500

    chore: update Google Tag Manager ID to GTM-5ZSN38DX

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -33,12 +33,12 @@ export default function RootLayout({
 new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
 j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
 'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-})(window,document,'script','dataLayer','GTM-NJ7235LM');`
+})(window,document,'script','dataLayer','GTM-5ZSN38DX');`
         }} />
       </head>
       <body className={`${poppins.className} font-sans antialiased text-gray-800 overflow-x-hidden w-full relative`}>
         <noscript dangerouslySetInnerHTML={{
-          __html: `<iframe src="https://www.googletagmanager.com/ns.html?id=GTM-NJ7235LM" height="0" width="0" style="display:none;visibility:hidden"></iframe>`
+          __html: `<iframe src="https://www.googletagmanager.com/ns.html?id=GTM-5ZSN38DX" height="0" width="0" style="display:none;visibility:hidden"></iframe>`
         }} />
         <Providers>
           <RadiusInitializer />
