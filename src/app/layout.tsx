a45842c59cf4a2fc66f93063f12f015b914ad8bb
Fyntryx <adnan@fyntryx.org>
2026-06-04 15:14:45 +0500

    feat: add Meta Pixel tracking code

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -35,10 +35,25 @@ j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
 'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
 })(window,document,'script','dataLayer','GTM-5ZSN38DX');`
         }} />
+        <script dangerouslySetInnerHTML={{
+          __html: `!function(f,b,e,v,n,t,s)
+{if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+n.queue=[];t=b.createElement(e);t.async=!0;
+t.src=v;s=b.getElementsByTagName(e)[0];
+s.parentNode.insertBefore(t,s)}(window, document,'script',
+'https://connect.facebook.net/en_US/fbevents.js');
+fbq('init', '1423539059415877');
+fbq('track', 'PageView');`
+        }} />
       </head>
       <body className={`${poppins.className} font-sans antialiased text-gray-800 overflow-x-hidden w-full relative`}>
         <noscript dangerouslySetInnerHTML={{
           __html: `<iframe src="https://www.googletagmanager.com/ns.html?id=GTM-5ZSN38DX" height="0" width="0" style="display:none;visibility:hidden"></iframe>`
+        }} />
+        <noscript dangerouslySetInnerHTML={{
+          __html: `<img height="1" width="1" style="display:none" src="https://www.facebook.com/tr?id=1423539059415877&ev=PageView&noscript=1" />`
         }} />
         <Providers>
           <RadiusInitializer />
