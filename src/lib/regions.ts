e9f136f1907dff740de36a7a812c0618487baf0f
Fyntryx <adnan@fyntryx.org>
2026-06-28 12:27:51 +0500

    chore: Remove Tier B suburbs from regions.ts to prevent 404s

--- src/lib/regions.ts
+++ src/lib/regions.ts
@@ -9,9 +9,7 @@ export const serviceRegions = [
     suburbs: [
       { name: "Melbourne CBD", slug: "melbourne-cbd", path: "/apartment-cleaning-melbourne-cbd" },
       { name: "North Melbourne", slug: "north-melbourne" },
-      { name: "Richmond", slug: "richmond" },
-      { name: "South Yarra", slug: "south-yarra" },
-      { name: "Windsor", slug: "windsor" }
+      { name: "South Yarra", slug: "south-yarra" }
     ]
   },
   {
@@ -20,19 +18,10 @@ export const serviceRegions = [
     iconProps: { direction: 'S' as const },
     iconClass: "text-[#FB8C42]",
     suburbs: [
-      { name: "Albert Park", slug: "albert-park" },
-      { name: "Armadale", slug: "armadale" },
-      { name: "Bentleigh East", slug: "bentleigh-east" },
       { name: "Brighton", slug: "brighton" },
-      { name: "Carnegie", slug: "carnegie" },
       { name: "Cheltenham", slug: "cheltenham" },
-      { name: "Glen Iris", slug: "glen-iris" },
       { name: "Hampton", slug: "hampton" },
-      { name: "Hawthorn", slug: "hawthorn" },
-      { name: "Kew", slug: "kew" },
       { name: "Malvern", slug: "malvern" },
-      { name: "Oakleigh", slug: "oakleigh" },
-      { name: "Sandringham", slug: "sandringham" },
       { name: "Toorak", slug: "toorak" }
     ]
   },
@@ -44,13 +33,7 @@ export const serviceRegions = [
     suburbs: [
       { name: "Brunswick", slug: "brunswick" },
       { name: "Bundoora", slug: "bundoora" },
-      { name: "Coburg", slug: "coburg" },
-      { name: "Craigieburn", slug: "craigieburn" },
-      { name: "Greensborough", slug: "greensborough" },
-      { name: "Ivanhoe", slug: "ivanhoe" },
-      { name: "Mernda", slug: "mernda" },
-      { name: "Preston", slug: "preston" },
-      { name: "Reservoir", slug: "reservoir" }
+      { name: "Preston", slug: "preston" }
     ]
   },
   {
@@ -58,19 +41,7 @@ export const serviceRegions = [
     icon: DirectionIcon,
     iconProps: { direction: 'E' as const },
     iconClass: "text-[#FB8C42]",
-    suburbs: [
-      { name: "Balwyn North", slug: "balwyn-north" },
-      { name: "Box Hill", slug: "box-hill" },
-      { name: "Camberwell", slug: "camberwell" },
-      { name: "Chelsea", slug: "chelsea" },
-      { name: "Croydon", slug: "croydon" },
-      { name: "Doncaster", slug: "doncaster" },
-      { name: "Doncaster East", slug: "doncaster-east" },
-      { name: "Glen Waverley", slug: "glen-waverley" },
-      { name: "Mount Waverley", slug: "mount-waverley" },
-      { name: "Ringwood", slug: "ringwood" },
-      { name: "Templestowe", slug: "templestowe" }
-    ]
+    suburbs: []
   },
   {
     region: "West & North-West",
@@ -78,17 +49,9 @@ export const serviceRegions = [
     iconProps: { direction: 'W' as const },
     iconClass: "text-[#FB8C42]",
     suburbs: [
-      { name: "Brooklyn", slug: "brooklyn" },
-      { name: "Caroline Springs", slug: "caroline-springs" },
       { name: "Essendon", slug: "essendon" },
-      { name: "Footscray", slug: "footscray" },
       { name: "Maribyrnong", slug: "maribyrnong" },
-      { name: "Moonee Ponds", slug: "moonee-ponds" },
-      { name: "Point Cook", slug: "point-cook" },
-      { name: "St Albans", slug: "st-albans" },
-      { name: "Strathmore", slug: "strathmore" },
-      { name: "Werribee", slug: "werribee" },
-      { name: "Yarraville", slug: "yarraville" }
+      { name: "Point Cook", slug: "point-cook" }
     ]
   }
 ];
