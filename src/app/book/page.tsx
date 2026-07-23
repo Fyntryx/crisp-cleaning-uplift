4b87ce16ba6d80e6ac52f3f472cb7a2fbeaa60f6
Fyntryx <adnan@fyntryx.org>
2026-07-23 16:47:22 +0500

    Convert px to rem without global clamp

--- src/app/book/page.tsx
+++ src/app/book/page.tsx
@@ -11,7 +11,7 @@ export default function BookPage() {
   return (
     <div className="flex flex-col min-h-screen bg-white">
       <MinimalNavbar />
-      <main className="flex-1 mt-[65px]">
+      <main className="flex-1 mt-[4.0625rem]">
         <BookingPageFlow hiddenInline={true} />
       </main>
     </div>
